ac23b9a67e9869e429b6ee121b0899f7f20fd928
Nayan singhai <nayansinghai66@gmail.com>
2020-05-07 09:10:45 +0530

    completely Responsive

--- resume.js
+++ resume.js
@@ -1,18 +1,27 @@
 function show()
 {
    
-    let pElement=document.getElementsByClassName(this.className)[1];
+    let cName = this.className;
+    let cIndex = cName.lastIndexOf(" ");
+    cName = cName.slice(cIndex);
+    let pElement = document.getElementsByClassName(cName)[1];
     pElement.innerHTML=this.value;
 
     
     
 }
-var textareas=document.getElementsByTagName("textarea");
+var inputs=document.getElementsByTagName("input");
+var textAreas=document.getElementsByTagName("textarea");
 
-for(let textarea of textareas )
+for( let textArea of textAreas)
 {
-    
-    textarea.addEventListener("keyup",show);
+    textArea.addEventListener("keyup",show);
+}
+
+for(let input of inputs )
+{
+    console.log(input);
+    input.addEventListener("keyup",show);
 
 }
 
